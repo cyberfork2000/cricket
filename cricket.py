7786faac6fd859001b3b03d2604cfc5e4c0fd777
Neil Banks <neil.banks@displaylink.com>
2020-09-25 14:12:38 +0100

    Added more players to dictionary.  Batsman have balls faced added

--- cricket.py
+++ cricket.py
@@ -4,10 +4,14 @@ import pprint
 # Bowler analysis = [Overs, Maidens, Runs,	Wickets, Economy]
 
 batsman = {1: {'name': 'Burns', 'runs': 0, '4s': 0, "6s": 0, "balls": 0},
-           2: {'name': 'Flint', 'runs': 0, '4s': 0, "6s": 0}}
+           2: {'name': 'Flint', 'runs': 0, '4s': 0, "6s": 0, "balls": 0},
+           3: {'name': 'Yardy', 'runs': 0, '4s': 0, "6s": 0, "balls": 0},
+           4: {'name': 'Gooch', 'runs': 0, '4s': 0, "6s": 0, "balls": 0}}
 
 bowlers = {1: {'name': 'Archer', 'overs': 0, 'maidens': 0, "runs": 0},
-           2: {'name': 'Broad', 'overs': 0, 'maidens': 0, "runs": 0}}
+           2: {'name': 'Broad', 'overs': 0, 'maidens': 0, "runs": 0},
+           3: {'name': 'Chase', 'overs': 0, 'maidens': 0, "runs": 0},
+           4: {'name': 'Holder', 'overs': 0, 'maidens': 0, "runs": 0}}
 
 print(batsman)
 print(batsman[1]['name'])
@@ -51,6 +55,7 @@ def boundary_scored(current_batsman, runs):
 def runs_scored_from_delivery(runs, current_batsman, current_bowler):
     current_bowler['runs'] = current_bowler['runs'] + runs
     current_batsman['runs'] = current_batsman['runs'] + runs
+    current_batsman['balls'] = current_batsman['balls'] + 1
     boundary_scored(current_batsman, runs)
     print("Bowler conceded is {}".format(current_bowler['runs']))
     print("Batsman has scored {}".format(current_batsman['runs']))
@@ -66,10 +71,11 @@ def swap_bowler(current_bowler, other_bowler):
 
 def show_scorecard():
     print("Scorecard")
-    print("Batsman / Runs / 4s / 6s")
+    print("Batsman / Runs / 4s / 6s / Balls")
     for batter in batsman:
         pprint.pprint(batsman[batter]['name'] + " " + str(batsman[batter]['runs'])
-                      + " " + str(batsman[batter]['4s']) + " " + str(batsman[batter]['6s']))
+                      + " " + str(batsman[batter]['4s']) + " " + str(batsman[batter]['6s'])
+                      + " " + str(batsman[batter]['balls']))
     print("===================================")
     print("Bowler / Overs / Runs")
     for bowler in bowlers:
@@ -113,6 +119,11 @@ def cricket():
         number_of_runs = int(input("score or -1 to end: "))
         if number_of_runs == -1:
             break
+        elif number_of_runs == -2:
+            print("wicket")
+            runs_scored_from_delivery(0, current_batsman, current_bowler)
+            # next batsman in
+            # credit bowler with wicket
         elif is_even(number_of_runs):  # 0 2 4 6
             runs_scored_from_delivery(number_of_runs, current_batsman, current_bowler)
         elif not is_even(number_of_runs):  # 1 3
